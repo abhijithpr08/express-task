4d79bca0a42bdebaf903165586f346ad2479c9ff
abhijithpr08 <abhith628@gmail.com>
2026-01-09 10:21:37 +0530

    a 404 response for a specific route

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const PORT = 3000
 
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
+// app.get("/",(req,res)=>{
+//     res.send("hello world")
+// })
 
 app.get("/greet/:name",(req,res)=>{
     res.send(`hello ${req.params.name}`)
@@ -21,6 +21,10 @@ res.json({status:"ok"})
 })
 
 app.use(express.static("public"))
+
+app.get("/notfound", (req,res)=>{
+    res.status(404).send("404 error: page not found")
+})
 
 app.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`)

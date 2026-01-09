3026115711942698c6278eb92e68418f7d444cbf
abhijithpr08 <abhith628@gmail.com>
2026-01-09 10:06:46 +0530

    dynamic greeting

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/",(req,res)=>{
     res.send("hello world")
 })
 
+app.get("/greet/:name",(req,res)=>{
+    res.send(`hello ${req.params.name}`)
+})
+
 app.listen(PORT,()=>{
     console.log(`server running at http://localhost:${PORT}`)
 })

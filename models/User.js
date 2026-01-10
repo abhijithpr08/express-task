afdaad6c079863ccb9fb3a4bf4454d77f133a3cb
abhijithpr08 <abhith628@gmail.com>
2026-01-10 23:27:26 +0530

    age limit change to 5

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
-    age:{type:Number, required:true,min:1} 
+    age:{type:Number, required:true,min:5} 
 })
 
 export default mongoose.model("User", userSchema)
